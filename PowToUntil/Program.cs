a896307bacd7b2e501bf92671805cd87cdc3612a
Julia Costa <juliacosta232005@gmail.com>
2024-03-08 10:29:27 +0000

    Added PowersOf2Until5() method

--- PowToUntil/Program.cs
+++ PowToUntil/Program.cs
@@ -8,5 +8,12 @@ namespace PowToUntil
         {
             Console.WriteLine("Hello, World!");
         }
+        private static void PowerOf2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i)
+            }
+        }
     }
 }
