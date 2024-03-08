a4d6883ddf124590a865c595f2ab6f53da4392e7
Julia Costa <juliacosta232005@gmail.com>
2024-03-08 01:16:14 +0000

    Added declaration of a Matrix and a Vector based on the given args

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace ArrayMult
 {
@@ -6,7 +7,17 @@ namespace ArrayMult
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declaração da matriz
+            float[,] matrix = new float[2,2];
+            matrix[0,0] = float.Parse(args[0]);
+            matrix[0,1] = float.Parse(args[1]);
+            matrix[1,0] = float.Parse(args[2]);
+            matrix[1,1] = float.Parse(args[3]);
+
+            // Declaração do vetor
+            float[] vector = new float[2];
+            vector[0] = float.Parse(args[4]);
+            vector[1] = float.Parse(args[5]);
         }
     }
 }
