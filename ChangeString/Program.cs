13379b209c94f44caecec7b4ae43d31af3dd719b
Julia Costa <juliacosta232005@gmail.com>
2024-03-08 00:39:02 +0000

    Added logic to print all string characters

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -9,9 +9,15 @@ namespace ChangeString
             Console.Write("Insira uma string: ");
             string s = Console.ReadLine();
 
-            Console.Write("\nInsira uma letra para ser substituída: ")
-            char c = Console.ReadLine()
+            Console.Write("\nInsira uma letra para ser substituída: ");
+            string c = Console.ReadLine();
 
+            // string[] newStringAr = new string[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+            }
         }
     }
 }
