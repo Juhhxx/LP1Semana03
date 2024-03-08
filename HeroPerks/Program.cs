3e54f81068e7db97783f2aa22b782c6005b84db3
Julia Costa <juliacosta232005@gmail.com>
2024-03-08 04:32:31 +0000

    Added print skills

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -8,23 +8,22 @@ namespace HeroPerks
         {
             Perks myRoles = 0000;
 
-            foreach (string input in args)
+            for(int i = 0; i < args.Length; i++)
             {
                 Perks addRole = 0000;
-
-                if (input == "w")
+                if (args[i] == "w")
                 {
                     addRole = Perks.WatherBreathing;
                 }
-                else if (input == "a")
+                else if (args[i] == "a")
                 {
                     addRole = Perks.AutoHeal;
                 }
-                else if (input == "s")
+                else if (args[i] == "s")
                 {
                     addRole = Perks.Stealth;
                 }
-                else if (input == "d")
+                else if (args[i] == "d")
                 {
                     addRole = Perks.DoubleJump;
                 }
@@ -32,22 +31,46 @@ namespace HeroPerks
                 myRoles ^= addRole;
             }
             
+            string allRoles = "";
 
             if ((myRoles & Perks.WatherBreathing) == Perks.WatherBreathing)
             { 
-                Console.WriteLine(Perks.WatherBreathing);   
+                allRoles += Perks.WatherBreathing.ToString(); 
+                if ((myRoles ^= Perks.WatherBreathing) != 0)
+                {
+                    allRoles += ", ";
+                }
             }
             if ((myRoles & Perks.Stealth) == Perks.Stealth)
             {
-                Console.WriteLine(Perks.Stealth);
+                allRoles += Perks.Stealth.ToString();
+                if ((myRoles ^= Perks.Stealth) != 0)
+                {
+                    allRoles += ", ";
+                }
             }
             if ((myRoles & Perks.AutoHeal) == Perks.AutoHeal)
             {
-                Console.WriteLine(Perks.AutoHeal);
+                allRoles += Perks.AutoHeal.ToString(); 
+                if ((myRoles ^= Perks.AutoHeal) != 0)
+                {
+                    allRoles += ", ";
+                }
             }
             if ((myRoles & Perks.DoubleJump) == Perks.DoubleJump)
             {
-                Console.WriteLine(Perks.DoubleJump);
+                allRoles += Perks.DoubleJump.ToString();
+            }
+        
+            Console.WriteLine(allRoles);
+
+            if ((myRoles & (Perks.Stealth & Perks.DoubleJump)) == (Perks.Stealth & Perks.DoubleJump))
+            {
+                Console.WriteLine("Silent Jumper!");
+            }
+            if ((myRoles & Perks.AutoHeal) == Perks.AutoHeal)
+            {
+                Console.WriteLine("Not gonna make it!");
             }
         }
     }
